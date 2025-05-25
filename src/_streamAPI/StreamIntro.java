9d7b86e5ca4f8ef534623b32d2b2c9b1d173280f
NishantRaj <nishant.read@gmail.com>
2025-05-25 11:09:48 +0530

    LenovoBat

---
+++ src/_streamAPI/StreamIntro.java
@@ -0,0 +1,18 @@
+package _streamAPI;
+
+import java.util.List;
+
+/**
+ * Sunday, May 25, 2025 11:08:50 AM
+ * 
+ */
+
+public class StreamIntro {
+	public static void main(String[] args) {
+		// How to Declare List ?
+		List<Integer> list1 = List.of(2, 4, 50, 21, 22, 67); // UnModifiable List
+		// list1.add(20); // java.lang.UnsupportedOperationException
+		System.out.println(list1);
+
+	}
+}
