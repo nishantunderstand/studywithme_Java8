e538d9e850660144daf45fe9525582987c9b8f1b
NishantRaj <nishant.read@gmail.com>
2025-05-25 11:16:34 +0530

    LenovoBat

--- src/_streamAPI/StreamIntro.java
+++ src/_streamAPI/StreamIntro.java
@@ -1,9 +1,25 @@
 package _streamAPI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Sunday, May 25, 2025 11:08:50 AM
+ * 
+ * List 
+ * 
+ * Types of List :
+ * 
+ * Types of List (Based on Mutablity) :
+ * 
+ * 1. Immutable
+ * List.of
+ * Arrays.asList
+ * 
+ * 2. Mutable 
+ * new ArrayList<>()
+ * 
  * 
  */
 
@@ -13,6 +29,18 @@ public class StreamIntro {
 		List<Integer> list1 = List.of(2, 4, 50, 21, 22, 67); // UnModifiable List
 		// list1.add(20); // java.lang.UnsupportedOperationException
 		System.out.println(list1);
+		
+		List<Integer> list2 = new ArrayList<Integer>();
+		list2.add(12);
+		list2.add(22);
+		list2.add(32);
+		list2.add(42);
+		System.out.println(list2); // Modifable List
+		
+		List<Integer> list3 = Arrays.asList(23,567,12,677,24); // UnModifable List
+		System.out.println(list3);
+		
+		
 
 	}
 }
