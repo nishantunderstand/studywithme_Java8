cfad5f26bf83fe38a57ef12a724c86cb0eca7cab
NishantRaj <nishant.read@gmail.com>
2025-06-03 15:57:15 +0530

    LenovoBat

---
+++ src/_java8_streamAPI_CodingQuestion/StreamTerminalImportance.java
@@ -0,0 +1,23 @@
+package _java8_streamAPI_CodingQuestion;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Jun 2, 2025 , 10:13:59 PM
+ * Streams in Java are lazy — meaning they only execute their intermediate operations (filter, map, etc.) when a terminal operation is invoked.
+Intermediate operations (like filter, map, sorted) are only setup steps.
+Terminal operations (like forEach, collect, count, anyMatch, etc.) trigger actual processing.
+ *
+ */
+
+
+public class StreamTerminalImportance {
+	public static void main(String[] args) {
+		
+		List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 32);
+		System.out.println(myList.stream().filter(n -> n % 2 == 0));
+		myList.stream().filter(n -> n % 2 == 0);
+		
+	}
+}
