0c52d2e1d7ba12811df979b5747dfaa42cafd36b
Nishant Raj <nishant.read@gmail.com>
2026-07-25 18:54:12 +0530

    LenovoBat 25-Jul-26 18:54:11.97

--- src/main/java/_java8_streamAPI_Question_JavaConceptOfDay_2/e_5_EmployeesJoinedAfter2015.java
+++ src/main/java/_java8_streamAPI_Question_JavaConceptOfDay_2/e_5_EmployeesJoinedAfter2015.java
@@ -1,5 +1,9 @@
 package _java8_streamAPI_Question_JavaConceptOfDay_2a;
 
+import java.util.stream.Collectors;
+
+import static _java8_streamAPI_Question_JavaConceptOfDay_2a.Main.employeeList;
+
 /**
 * @see <a href="https://javaconceptoftheday.com/solving-real-time-queries-using-java-8-features-employee-management-system/">
 *     Java 8 Interview Sample Coding Questions </a>
@@ -8,5 +12,8 @@ public class e_5_EmployeesJoinedAfter2015 {
 
     public static void main(String[] args) {
 
+        employeeList.stream()
+                .filter(emp -> emp.Employee::getYearOfJoining>2015)
+                .forEach((k,v)-> System.out.println(k+ "-> "+ v));
     }
 }
