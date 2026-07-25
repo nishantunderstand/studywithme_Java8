92f92ac195e4f85a665ebb5f1b64c2d38748602e
Nishant Raj <nishant.read@gmail.com>
2026-07-25 18:20:10 +0530

    New Stream Question

---
+++ src/main/java/_java8_streamAPI_Question_JavaConceptOfDay_2/a_1_CountMaleAndFemaleEmployees.java
@@ -0,0 +1,32 @@
+package _java8_streamAPI_Question_JavaConceptOfDay_2;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static _java8_streamAPI_Question_JavaConceptOfDay_2.Main.employeeList;
+
+/**
+* @see <a href="https://javaconceptoftheday.com/solving-real-time-queries-using-java-8-features-employee-management-system/">
+*     Java 8 Interview Sample Coding Questions </a>
+*/
+public class a_1_CountMaleAndFemaleEmployees {
+
+    public static void main(String[] args) {
+        long male = employeeList.stream().filter(emp -> emp.getGender().equals("Male")).count();
+        System.out.println("Male  : "+male);
+
+
+        long female = employeeList.stream().filter(emp -> emp.getGender().equals("Female")).count();
+        System.out.println("female  : "+female);
+
+        System.out.println("====== Saturday, July 25, 2026 4:23:01 PM ======");
+
+        Map<Employee, Long> noOfMaleAndFemale = employeeList.stream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+        System.out.println(noOfMaleAndFemale);
+    }
+}
---
+++ src/main/java/_java8_streamAPI_Question_JavaConceptOfDay_2/o_15_OldestEmployeeDetails.java
@@ -0,0 +1,12 @@
+package _java8_streamAPI_Question_JavaConceptOfDay_2;
+
+/**
+* @see <a href="https://javaconceptoftheday.com/solving-real-time-queries-using-java-8-features-employee-management-system/">
+*     Java 8 Interview Sample Coding Questions </a>
+*/
+public class o_15_OldestEmployeeDetails {
+
+    public static void main(String[] args) {
+
+    }
+}
