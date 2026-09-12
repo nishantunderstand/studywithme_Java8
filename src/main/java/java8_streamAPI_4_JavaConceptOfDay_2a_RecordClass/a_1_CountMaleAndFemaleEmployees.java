b271fb726db8e1a1268fcc14ad00b08af68aca9a
Nishant Raj <nishant.read@gmail.com>
2026-09-12 16:54:07 +0530

    Saturday, September 12, 2026 4:03:16 PM

--- src/main/java/java8_streamAPI_4_JavaConceptOfDay_2a_RecordClass/a_1_CountMaleAndFemaleEmployees.java
+++ src/main/java/java8_streamAPI_4_JavaConceptOfDay_2a_RecordClass/a_1_CountMaleAndFemaleEmployees.java
@@ -1,33 +1,25 @@
 package java8_streamAPI_4_JavaConceptOfDay_2a_RecordClass;
 
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import static java8_streamAPI_4_JavaConceptOfDay_2a_RecordClass.Main.employeeList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
 * @see <a href="https://javaconceptoftheday.com/solving-real-time-queries-using-java-8-features-employee-management-system/">
 *     Java 8 Interview Sample Coding Questions </a>
 */
-public class a_1_CountMaleAndFemaleEmployees {
+record Employee(int id, String name, int age ,String gender, String department,int yearOfJoining, double salary) {
+}
 
+class a_1_CountMaleAndFemaleEmployees {
     public static void main(String[] args) {
-        long male = employeeList.stream().filter(emp -> emp.getGender().equals("Male")).count();
-        System.out.println("Male  : "+male);
+        List<Employee> empList = Arrays.asList(
+                new Employee(11, "Aman", 25, "M", "IT", 2022, 75000),
+                new Employee(20, "Aman Kumar", 28, "M", "HR", 2020, 65000),
+                new Employee(12, "Anmanika", 24, "F", "IT", 2023, 70000),
+                new Employee(13, "Anmanika Jee", 30, "F", "Finance", 2019, 80000)
+        );
+        empList.stream().forEach(System.out::println);
 
-
-        long female = employeeList.stream().filter(emp -> emp.getGender().equals("Female")).count();
-        System.out.println("female  : "+female);
-
-        System.out.println("====== Saturday, July 25, 2026 4:23:01 PM ======");
-
-        Map<Employee1, Long> noOfMaleAndFemale = employeeList.stream()
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
-        System.out.println(noOfMaleAndFemale);
     }
 }
